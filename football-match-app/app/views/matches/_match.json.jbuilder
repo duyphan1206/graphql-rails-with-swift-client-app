900e1b5ceb38432ffd12ce3afe25c07a67de86f5
Duy Phan <duy.phan@asnet.com.vn>
2017-11-02 09:27:48 +0700

    Remove un-used folders

--- football-match-app/app/views/matches/_match.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! match, :id, :title, :description, :score, :created_at, :updated_at
-json.url match_url(match, format: :json)
--- football-match-app/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-
-  get 'welcome/index'
-
-  root 'welcome#index'
-
-  resources :matches
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
